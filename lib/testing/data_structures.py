b342a0f3282aef3188ca2c92ae6fdfe047f09fb7
dicksonmutwiriguantai <dickson.mutwiri@student.moringaschool.com>
2023-12-19 23:48:20 +0300

    all passed

---
+++ lib/testing/data_structures.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+spicy_foods = [
+    {
+        "name": "Green Curry",
+        "cuisine": "Thai",
+        "heat_level": 9,
+    },
+    {
+        "name": "Buffalo Wings",
+        "cuisine": "American",
+        "heat_level": 3,
+    },
+    {
+        "name": "Mapo Tofu",
+        "cuisine": "Sichuan",
+        "heat_level": 6,
+    },
+]
+
+def get_names(spicy_foods):
+
+    return [food["name"]for food in spicy_foods]
+
+
+
+
+def get_spiciest_foods(spicy_foods):
+    return [food for food in spicy_foods if food["heat_level"] > 5]
+
+
+def print_spicy_foods(spicy_foods):
+     for food in spicy_foods:
+        print(f'{food["name"]} ({food["cuisine"]}) | Heat Level: {"🌶" * food["heat_level"]}')
+
+def get_spicy_food_by_cuisine(spicy_foods, cuisine):
+    for food in spicy_foods:
+        if cuisine == food["cuisine"]:
+            return food
+
+def print_spiciest_foods(spicy_foods):
+     for food in spicy_foods:
+        heat_level = "🌶" * food["heat_level"]
+
+        if food["heat_level"] >5:
+            print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}")
+
+def get_average_heat_level(spicy_foods):
+    total_heat_levels = 0
+    for food in spicy_foods:
+        total_heat_levels= total_heat_levels + food["heat_level"]
+    return total_heat_levels/len(spicy_foods)
+
+
+def create_spicy_food(spicy_foods, spicy_food):
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+
--- lib/testing/data_structures_test.py
+++ lib/testing/data_structures_test.py
@@ -1,4 +1,6 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 
 from data_structures import get_names, get_spiciest_foods, print_spicy_foods,\
                                 create_spicy_food, get_spicy_food_by_cuisine, \
